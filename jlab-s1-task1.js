a42e306e780080031d495960ded31409cf32e6d6
satendra <satendrakr.7@gmail.com>
2021-05-12 12:54:41 +0530

    renamed

--- jlab-s1-task1.js
+++
@@ -1,7 +0,0 @@
-const numbers = [1,7,9,12];
-
-const squareOfEachNumber = (numbers) => {
-   return numbers.map(number =>  {return number*number})
-    
-}
-console.log(squareOfEachNumber(numbers));
