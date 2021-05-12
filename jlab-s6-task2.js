3c1e98b66c312c7d59893543f6f3d788e5cb2f7d
satendra <satendrakr.7@gmail.com>
2021-05-12 16:52:59 +0530

    add solution to decode the string for 0 and 1

---
+++ jlab-s6-task2.js
@@ -0,0 +1,37 @@
+const decodeFor0 = (word,number) => {
+    let encodedWord = "";
+    if(number == 0){
+        for (index = 0; index < word.length; index++) {
+            if (word.charAt(index) == '#') {
+                encodedWord += 'a'
+            } else if (word.charAt(index) == '*') {
+                encodedWord += 'e'
+            } else {
+                encodedWord += word.charAt(index);
+            }
+        }
+        return encodedWord;
+    }
+   
+}
+
+const decodeFor1 = (word) => {
+    let encodedWord = "";
+    for (index = 0; index < word.length; index++) {
+        if (word.charAt(index) == '#') {
+            encodedWord += 'a'
+        } else if (word.charAt(index) == '*') {
+            encodedWord += 'e'
+        } else {
+            encodedWord += word.charAt(index);
+        }
+
+    }
+    return encodedWord;
+}
+
+const decode = (word,number) => {
+   return number == 0 ? decodeFor0(word) : decodeFor1(word);
+} 
+
+console.log(decode("s*l#d" ,1));
